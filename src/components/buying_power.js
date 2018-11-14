ca5bd234707984dd3802c5e29de275b7b28350b2
Dan Spector <danno2423@gmail.com>
2018-11-13 22:27:25 -0500

    got bars positioned correctly and added source map

--- src/components/buying_power.js
+++ src/components/buying_power.js
@@ -6,77 +6,77 @@ import * as d3Array from 'd3-array';
 
 let items = [
   {
-    name: 'Cup of Coffee',
+    name: 'cup of coffee',
     year: 1910,
     price: .05
   },
   {
-    name: 'Cup of Coffee',
+    name: 'cup of coffee',
     year: 1960,
     price: .20
   },
   {
-    name: 'Cup of Coffee',
+    name: 'cup of coffee',
     year: 2010,
     price: 1.50
   },
   {
-    name: `Hershey's Chocolate Bar`,
+    name: `Hershey's chocolate bar`,
     year: 1910,
     price: .02
   },
   {
-    name: `Hershey's Chocolate Bar`,
+    name: `Hershey's chocolate bar`,
     year: 1960,
     price: .05
   },
   {
-    name: `Hershey's Chocolate Bar`,
+    name: `Hershey's chocolate bar`,
     year: 2010,
     price: .99
   },
   {
-    name: 'Postage Stamp',
+    name: 'postage stamp',
     year: 1910,
     price: .02
   },
   {
-    name: 'Postage Stamp',
+    name: 'postage stamp',
     year: 1960,
     price: .04
   },
   {
-    name: 'Postage Stamp',
+    name: 'postage stamp',
     year: 2010,
     price: .44
   },
   {
-    name: 'New York Times Weekday Edition',
+    name: 'New York Times weekday',
     year: 1910,
     price: .01
   },
   {
-    name: 'New York Times Weekday Edition',
+    name: 'New York Times weekday',
     year: 1960,
     price: .05
   },
   {
-    name: 'New York Times Weekday Edition',
+    name: 'New York Times weekday',
     year: 2010,
     price: 2.00
   },
   {
-    name: 'Gallon of Gas',
+    name: 'gallon of gas',
     year: 1910,
     price: .12
   },
   {
-    name: 'Gallon of Gas',
+    name: 'gallon of gas',
     year: 1960,
     price: .30
   },
   {
-    name: 'Gallon of Gas',
+    name: 'gallon of gas',
     year: 2010,
     price: 2.80
   },
@@ -88,12 +88,6 @@ items = items.map(d => {
   d.buying_power = Math.round((.01 / d.price) * 1000) / 1000;
   return d;
 })
-
-console.log(items);
-
-//grouping items by year
-
-items = d3Array.group(items, d => d.year);
 
 console.log(items);
 
@@ -113,9 +107,11 @@ const svgDimensions = {
   height: document.querySelector('.svg').clientHeight
 }
 
+console.log(svgDimensions);
+
 function drawBuyingPower(item) {
   root_svg.append('text')
-    .text(`How far does my penny go when buying a ${item}?`)
+    .text(`How far does one penny go when buying a ${item}?`)
     .attr('x', svgDimensions.width * .5)
     .attr('y', svgDimensions.height *.12)
     .attr('text-anchor', 'middle')
@@ -150,22 +146,56 @@ function drawBuyingPower(item) {
   drawYearText('1960', svgDimensions.width * .5, 'green');
   drawYearText('2010', svgDimensions.width * .85, 'blue');
 
-  //append overall bar
-  for (let i = 1; i <= 3; i++) {
+  //append overall bars
+  for (let i = 0; i <= 2; i++) {
 
     console.log('hello')
     root_svg.append('rect')
       .attr('class', 'overallBar')
-      .attr('x', svgDimensions.width * (.075 +(.35 * (i-1)))) //the initial starting point is the first number. the second number is how much space there should be between the bars.
+      .attr('x', svgDimensions.width * (.075 +(.35 * i))) //the initial starting point is the first number. the second number is how much space there should be between the bars.
       .attr('y', svgDimensions.height * .35)
       .attr('width', svgDimensions.width * .15)
-      .attr('height', svgDimensions.height *.4 )
+      .attr('height', svgDimensions.height * .4)
       .style('fill', 'lightgray');
     }
+
+  //filters the array by the current item we're analyzing
+  items = items.filter(d => {
+    return d.name === item; 
+  });
+
+  console.log(items);
+
+  const buyingPowerScale = d3.scaleLinear()
+    .domain([0,1])
+    .range([0, svgDimensions.height * .4]);  
+
+  root_svg
+    .selectAll('.buyingPowerBar')
+    .data(items)
+    .enter()
+    .append('rect')
+    .attr('class', '.buyingPowerBar')
+    .attr('x', (d, i) => {
+      return svgDimensions.width * (.075 +(.35 * i))
+    })
+    .attr('y', d => {
+      return (svgDimensions.height * .4 - buyingPowerScale(d.buying_power)) + svgDimensions.height * .35 ; // the top of each bar is a relationship between the height and corresponding data value
+    })
+    // .attr('y', svgDimensions.height * .35)
+    .attr('width', svgDimensions.width * .15)
+    .attr('height', d => buyingPowerScale(d.buying_power))
+    .style('fill', 'black');
+
+    console.log('yo')
+
+
+// //grouping items by year
+
+// items = d3Array.group(items, d => d.year);
 
 
 
 }
 
 drawBuyingPower('cup of coffee');
-  
